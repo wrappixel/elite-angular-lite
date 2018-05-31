88c55e5d96e013e9a480aabd4616f2707c171a59
vishal-wrap <31442586+vishal-wrap@users.noreply.github.com>
2018-05-31 11:55:39 +0530

    ''

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AuthGuard } from './shared/guard/auth.guard';
 
 const routes: Routes = [
     {
         path: '',
-        loadChildren: './pages/pages.module#PagesModule',
-        canActivate: [AuthGuard]
+        loadChildren: './pages/pages.module#PagesModule'
     },
-    { path: 'login', loadChildren: './login/login.module#LoginModule' },
-    { path: 'signup', loadChildren: './signup/signup.module#SignupModule' },
-    { path: '404', loadChildren: './404/not-found.module#NotFoundModule' },
-    { path: '**', redirectTo: '404' }
+    { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes), NgbModule.forRoot()],
+    imports: [RouterModule.forRoot(routes, { useHash: true }), NgbModule.forRoot()],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
