63ad5a53face3241c8ee822c784ca41b0d976d81
skchhatraliya@gmail.com <skchhatraliya@gmail.com>
2020-07-13 12:19:57 +0530

    ''

---
+++ angular-10/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { FullComponent } from './layouts/full/full.component';
+
+export const Approutes: Routes = [
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+      { path: '', redirectTo: '/starter', pathMatch: 'full' },
+      {
+        path: 'starter',
+        loadChildren: () => import('./starter/starter.module').then(m => m.StarterModule)
+      },
+      {
+        path: 'component',
+        loadChildren: () => import('./component/component.module').then(m => m.ComponentsModule)
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '/starter'
+  }
+];
